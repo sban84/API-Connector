8aa243ecf8e42eff66159b67cc16a5c2722ccb4e
Suman Banerjee <java2suman@gmail.com>
2022-01-26 17:19:11 +0100

    intial-commit

---
+++ src/erisite_connector/main.py
@@ -0,0 +1,11 @@
+from util.calculator import add
+
+
+def main():
+    print("This is inside the main method")
+    res = add(2, 3)
+    print(f"The result is {res}")
+
+
+if __name__ == "__main__":
+    main()
